5028bfbf41c4d51c43a78ca0adf5fab070e3b21a
piperun <nevadadirren@gmail.com>
2024-08-28 12:16:02 +0200

    forgor 💀 init_db test that also passes

---
+++ integration_test/init_db.dart
@@ -0,0 +1,54 @@
+import 'dart:io';
+
+import 'package:chenron/data_struct/item.dart';
+import 'package:chenron/data_struct/metadata.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:chenron/database/database.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  late AppDatabase database;
+
+  setUp(() {
+    database = AppDatabase(queryExecutor: NativeDatabase.memory());
+  });
+  tearDown(() async {
+    await database.close();
+  });
+
+  test('AppDatabase constructor initializes with no setup', () {
+    final db = AppDatabase(queryExecutor: NativeDatabase.memory());
+    expect(db.schemaVersion, equals(1));
+  });
+
+  // Helper function to remove the database
+  Future<void> removeDatabaseFile(String databaseName) async {
+    final databasePath = await getApplicationDocumentsDirectory();
+    final file = File(databasePath.path + databaseName);
+    if (await file.exists()) {
+      await file.delete();
+    }
+  }
+
+  test('AppDatabase constructor with setupOnInit initializes item types',
+      () async {
+    final setupDatabase =
+        AppDatabase(databaseName: "test_database_setup", setupOnInit: true);
+
+    final itemTypes = await setupDatabase.select(setupDatabase.itemTypes).get();
+    final metadataTypes =
+        await setupDatabase.select(setupDatabase.metadataTypes).get();
+    print("item: $itemTypes\n metadata: $metadataTypes");
+    expect(itemTypes.length, equals(FolderItemType.values.length),
+        reason: 'Number of item types should match the enum values');
+    expect(metadataTypes.length, equals(MetadataTypeEnum.values.length),
+        reason: 'Number of metadata types should match the enum values');
+
+    await database.close();
+    await removeDatabaseFile('test_database_setup');
+  });
+}
