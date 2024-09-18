349a61cacc9c35fbfd9129abd9a3673286222c70
piperun <nevadadirren@gmail.com>
2024-09-18 11:30:51 +0200

    added convert

---
+++ lib/convert/folder_item_convert.dart
@@ -0,0 +1,30 @@
+import 'package:chenron/data_struct/item.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+String convertItemToString(ItemContent content) {
+  switch (content) {
+    case StringContent stringContent:
+      return stringContent.value;
+    case MapContent mapContent:
+      return mapContent.value['title'] ?? '';
+    default:
+      return '';
+  }
+}
+
+Function() getLaunchFunc(ItemContent content) {
+  switch (content) {
+    case StringContent stringContent:
+      return () => _launchURL(Uri.parse(stringContent.value));
+    default:
+      return () {};
+  }
+}
+
+void _launchURL(Uri url) async {
+  if (await canLaunchUrl(url)) {
+    await launchUrl(url);
+  } else {
+    print('Could not launch $url');
+  }
+}
