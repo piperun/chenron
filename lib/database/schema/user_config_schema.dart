ff35912dda5038024c70e7a7b3223b243a4f8a37
piperun <nevadadirren@gmail.com>
2025-03-11 17:39:58 +0100

    feat: Add primary keys and foreign key for UserConfigs relationship
    This commit adds primary keys to the `UserConfigs`, `BackupSettings`, and `ArchiveSettings` tables. It also adds a foreign key relationship between `BackupSettings` and `ArchiveSettings` to `UserConfigs`. This change establishes the one-to-one relationship between these tables, as suggested by the TODO comment.
    
    Key changes:
    
    - Added `primaryKey` to `UserConfigs`, `BackupSettings`, and `ArchiveSettings` tables.
    - Added `userConfigId` as a foreign key in `BackupSettings` and `ArchiveSettings` referencing `UserConfigs`.

--- lib/database/schema/user_config_schema.dart
+++ lib/database/schema/user_config_schema.dart
@@ -1,26 +1,35 @@
 import "package:drift/drift.dart";
 
-// TODO: One-to-One relationship between UserConfigs, BackupSettings and ArhicveSettings
 class UserConfigs extends Table {
   TextColumn get id => text().withLength(min: 30, max: 60)();
   BoolColumn get darkMode => boolean().withDefault(const Constant(false))();
   BoolColumn get copyOnImport => boolean().withDefault(const Constant(true))();
+
   BoolColumn get archiveEnabled =>
       boolean().withDefault(const Constant(true))();
   TextColumn get colorScheme => text().nullable()();
-  TextColumn get archiveOrgS3AccessKey => text().nullable()();
-  TextColumn get archiveOrgS3SecretKey => text().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {id};
 }
 
 class BackupSettings extends Table {
   TextColumn get id => text().withLength(min: 30, max: 60)();
+  TextColumn get userConfigId => text().references(UserConfigs, #id)();
   TextColumn get backupInterval => text().nullable()();
   TextColumn get backupFilename => text().nullable()();
   TextColumn get backupPath => text().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {id};
 }
 
 class ArchiveSettings extends Table {
   TextColumn get id => text().withLength(min: 30, max: 60)();
+  TextColumn get userConfigId => text().references(UserConfigs, #id)();
   TextColumn get archiveOrgS3AccessKey => text().nullable()();
   TextColumn get archiveOrgS3SecretKey => text().nullable()();
+
+  @override
+  Set<Column> get primaryKey => {id};
 }
