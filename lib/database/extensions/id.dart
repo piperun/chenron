5583183a6638982e281193cfb0b78d542d6a71ba
piperun <nevadadirren@gmail.com>
2025-03-12 16:33:41 +0100

    feat: Add ConfigIdGeneratorExtension for generating IDs
    This commit adds a `ConfigIdGeneratorExtension` to the `ConfigDatabase` to generate unique IDs using `cuidSecure`. This extension mirrors the existing `IdGeneratorExtension` for the `AppDatabase`, providing a consistent approach to ID generation across different database instances. This change ensures that IDs are generated correctly within the context of the `ConfigDatabase`.

--- lib/database/extensions/id.dart
+++ lib/database/extensions/id.dart
@@ -1,7 +1,15 @@
 import "package:chenron/database/database.dart";
 import "package:cuid2/cuid2.dart";
 
+//TODO: Generalize this to all databases
+
 extension IdGeneratorExtension on AppDatabase {
+  String generateId() {
+    return cuidSecure(AppDatabase.idLength);
+  }
+}
+
+extension ConfigIdGeneratorExtension on ConfigDatabase {
   String generateId() {
     return cuidSecure(AppDatabase.idLength);
   }
