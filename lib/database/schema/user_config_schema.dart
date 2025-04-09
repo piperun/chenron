d26810871b6bbd7fd233beffefd1d797bd7366f5
piperun <nevadadirren@gmail.com>
2025-04-09 13:33:19 +0200

    feat: Add user theme and archive.org S3 config to database schema
    This commit extends the database schema to include user theme support and configuration options for Archive.org S3 integration.
    
    - Adds a `UserThemes` table to store user-defined themes, including ID, user config ID, name, and theme data.
    - Adds `archiveOrgS3AccessKey` and `archiveOrgS3SecretKey` columns to the `UserConfigs` table for storing Archive.org S3 credentials.
    - Adds `lastBackupTimestamp` column to the `BackupSettings` table to track the last backup time.

--- lib/database/schema/user_config_schema.dart
+++ lib/database/schema/user_config_schema.dart
@@ -5,10 +5,22 @@ class UserConfigs extends Table {
   BoolColumn get darkMode => boolean().withDefault(const Constant(false))();
   BoolColumn get copyOnImport => boolean().withDefault(const Constant(true))();
 
+  TextColumn get archiveOrgS3AccessKey => text().nullable()();
+  TextColumn get archiveOrgS3SecretKey => text().nullable()();
+
   BoolColumn get archiveEnabled =>
       boolean().withDefault(const Constant(true))();
   TextColumn get colorScheme => text().nullable()();
 
+  @override
+  Set<Column> get primaryKey => {id};
+}
+
+class UserThemes extends Table {
+  TextColumn get id => text().withLength(min: 30, max: 60)();
+  TextColumn get userConfigId => text().references(UserConfigs, #id)();
+  TextColumn get name => text().unique()();
+  TextColumn get theme => text().nullable()();
   @override
   Set<Column> get primaryKey => {id};
 }
@@ -19,6 +31,7 @@ class BackupSettings extends Table {
   TextColumn get backupInterval => text().nullable()();
   TextColumn get backupFilename => text().nullable()();
   TextColumn get backupPath => text().nullable()();
+  DateTimeColumn get lastBackupTimestamp => dateTime().nullable()();
 
   @override
   Set<Column> get primaryKey => {id};
