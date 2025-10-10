b3a37a0119d938984e479fa46097d88259b2bf1d
piperun <nevadadirren@gmail.com>
2025-10-11 01:38:04 +0200

    Refactor RootPage to replace bottom sheet with modal dialog for item creation and remove unused components

--- apps/chenron/lib/features/shell/pages/root.dart
+++ apps/chenron/lib/features/shell/pages/root.dart
@@ -1,11 +1,10 @@
-import "package:chenron/features/shell/ui/sections/appbar_section.dart";
 import "package:flutter/material.dart";
-import "package:chenron/features/create/folder/pages/create_folder.dart";
-import "package:chenron/features/create/link/pages/create_link.dart";
 import "package:chenron/shared/search/searchbar.dart";
-import "package:chenron/shared/ui/dark_mode.dart";
-import "package:chenron/utils/logger.dart";
+import "package:chenron/features/shell/widgets/add_item_modal.dart";
 import "package:chenron/features/shell/ui/sections/navigation_section.dart";
+import "package:chenron/features/shell/ui/sections/appbar_section.dart";
+import "package:chenron/utils/logger.dart";
+import "package:chenron/shared/ui/dark_mode.dart";
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
@@ -18,39 +17,10 @@ class _RootPageState extends State<RootPage> {
   AppPage _currentPage = AppPage.dashboard;
   bool _isExtended = true;
 
-  void _showCreateBottomSheet() {
-    showModalBottomSheet(
+  void _showCreateModal() {
+    showDialog(
       context: context,
-      shape: const RoundedRectangleBorder(
-        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-      ),
-      builder: (context) => _CreateBottomSheet(
-        onFolderTap: () {
-          Navigator.pop(context);
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const CreateFolderPage()),
-          );
-        },
-        onLinkTap: () {
-          Navigator.pop(context);
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => const CreateLinkPage()),
-          );
-        },
-        onDocumentTap: () {
-          Navigator.pop(context);
-          _showDocumentComingSoon();
-        },
-      ),
-    );
-  }
-
-  void _showDocumentComingSoon() {
-    loggerGlobal.warning("RootPage", "Document creation not implemented yet.");
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(content: Text("Document creation coming soon!")),
+      builder: (context) => const AddItemModal(),
     );
   }
 
@@ -96,7 +66,7 @@ class _RootPageState extends State<RootPage> {
             selectedIndex: _selectedNavIndex,
             onDestinationSelected: _onDestinationSelected,
             onToggleExtended: () => setState(() => _isExtended = !_isExtended),
-            onAddPressed: _showCreateBottomSheet,
+            onAddPressed: _showCreateModal,
           ),
           const VerticalDivider(thickness: 1, width: 1),
           Expanded(child: _currentPage.page),
@@ -213,87 +183,6 @@ class _AddButton extends StatelessWidget {
       onPressed: onPressed,
       tooltip: "Add New",
       child: const Icon(Icons.add),
-    );
-  }
-}
-
-// Create Bottom Sheet Component
-class _CreateBottomSheet extends StatelessWidget {
-  final VoidCallback onFolderTap;
-  final VoidCallback onLinkTap;
-  final VoidCallback onDocumentTap;
-
-  const _CreateBottomSheet({
-    required this.onFolderTap,
-    required this.onLinkTap,
-    required this.onDocumentTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(24.0),
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          const Text(
-            "Create New",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            "Choose what you'd like to add",
-            style: TextStyle(
-                color: Theme.of(context).colorScheme.onSurfaceVariant),
-          ),
-          const SizedBox(height: 24),
-          _CreateOption(
-            icon: Icons.create_new_folder_outlined,
-            title: "Folder",
-            subtitle: "Create a new folder",
-            onTap: onFolderTap,
-          ),
-          _CreateOption(
-            icon: Icons.add_link,
-            title: "Link",
-            subtitle: "Add a link or bookmark",
-            onTap: onLinkTap,
-          ),
-          _CreateOption(
-            icon: Icons.description_outlined,
-            title: "Document",
-            subtitle: "Upload a document",
-            onTap: onDocumentTap,
-          ),
-          const SizedBox(height: 16),
-        ],
-      ),
-    );
-  }
-}
-
-// Create Option Component
-class _CreateOption extends StatelessWidget {
-  final IconData icon;
-  final String title;
-  final String subtitle;
-  final VoidCallback onTap;
-
-  const _CreateOption({
-    required this.icon,
-    required this.title,
-    required this.subtitle,
-    required this.onTap,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      leading: Icon(icon, size: 32),
-      title: Text(title),
-      subtitle: Text(subtitle),
-      onTap: onTap,
     );
   }
 }
