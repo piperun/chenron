9f53e68a06d9a81a86dc75a26b1bdecf21b67e7c
piperun <nevadadirren@gmail.com>
2025-11-21 23:18:52 +0100

    feat: Update TestPathProvider formatting for improved readability

--- apps/chenron/lib/test_support/path_provider_fake.dart
+++ apps/chenron/lib/test_support/path_provider_fake.dart
@@ -1,12 +1,17 @@
+// ignore_for_file: depend_on_referenced_packages, invalid_use_of_visible_for_testing_member
 import "dart:io";
 
 import "package:path_provider_platform_interface/path_provider_platform_interface.dart";
 import "package:plugin_platform_interface/plugin_platform_interface.dart";
 import "package:flutter_test/flutter_test.dart";
 
-class TestPathProvider extends Fake with MockPlatformInterfaceMixin implements PathProviderPlatform {
+class TestPathProvider extends Fake
+    with MockPlatformInterfaceMixin
+    implements PathProviderPlatform {
   final Directory base;
-  TestPathProvider({String? basePath}) : base = Directory(basePath ?? Directory.systemTemp.createTempSync("chenron_test_base").path) {
+  TestPathProvider({String? basePath})
+      : base = Directory(basePath ??
+            Directory.systemTemp.createTempSync("chenron_test_base").path) {
     if (!base.existsSync()) {
       base.createSync(recursive: true);
     }
