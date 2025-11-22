abdfa217e674378b681b69fafd6c596fe7d1163f
piperun <nevadadirren@gmail.com>
2025-11-22 20:18:45 +0100

    feat: Add ArchiveOrgClient and options for interacting with the Wayback Machine API
    Cleaned up dangling files that aren't in use anymore.

--- packages/web_archiver/lib/src/archive_org/archive_org.dart
+++ packages/web_archiver/lib/src/archive_org/archive_org.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:logging/logging.dart';
-import 'package:web_archiver/src/archive_org_options.dart';
+import 'package:web_archiver/src/archive_org/archive_org_options.dart';
 
 /// Factory definition for creating an [ArchiveOrgClient].
 typedef ArchiveOrgClientFactory = ArchiveOrgClient Function(
--- packages/web_archiver/lib/web_archiver.dart
+++ packages/web_archiver/lib/web_archiver.dart
@@ -4,6 +4,6 @@ library;
 export "package:web_archiver/src/monolith/monolith_downloader.dart";
 export "package:web_archiver/src/monolith/monolith_runner.dart";
 export "package:web_archiver/src/archive_is/archive_is.dart";
-export "package:web_archiver/src/archive_org.dart";
-export "package:web_archiver/src/archive_org_options.dart";
+export "package:web_archiver/src/archive_org/archive_org.dart";
+export "package:web_archiver/src/archive_org/archive_org_options.dart";
 export "package:web_archiver/src/parse_archive_date.dart";
