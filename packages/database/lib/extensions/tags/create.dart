46cc017a95afe582da2c88d132608184cb60cf2b
piperun <nevadadirren@gmail.com>
2025-11-28 12:24:58 +0100

    refactor(tags): update addTag method to accept optional color parameter

--- packages/database/lib/extensions/tags/create.dart
+++ packages/database/lib/extensions/tags/create.dart
@@ -2,13 +2,11 @@ import "package:database/database.dart";
 import "package:database/operations/tag/tag_create_vepr.dart";
 
 extension TagExtensions on AppDatabase {
-  Future<String> addTag(String tagName) async {
+  Future<String> addTag(String tagName, {int? color}) async {
     final operation = TagCreateVEPR(this);
-    final input = (tagName: tagName);
+    final input = (tagName: tagName, color: color);
 
     final result = await operation.run(input);
     return result.tagId;
   }
 }
-
-
