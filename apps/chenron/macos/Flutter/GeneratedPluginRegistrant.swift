11796225307884dbc96bfcb8c2f3caee442b9e32
piperun <nevadadirren@gmail.com>
2026-02-16 13:29:54 +0100

    feat: add window_manager for persistent window sizing
    Restore the user's last window size on startup, clamped to the
    current screen bounds. On first run, default to 80% of screen.
    
    - Add window_manager dependency
    - Create WindowService for SharedPreferences read/write
    - Init window_manager in main() with waitUntilReadyToShow to
      prevent native-size flash
    - Persist size on resize and close via WindowListener
    - Add 12 unit tests for WindowService and computeTargetSize

--- apps/chenron/macos/Flutter/GeneratedPluginRegistrant.swift
+++ apps/chenron/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,15 +6,19 @@ import FlutterMacOS
 import Foundation
 
 import file_picker
+import screen_retriever_macos
 import shared_preferences_foundation
 import sqflite_darwin
 import sqlite3_flutter_libs
 import url_launcher_macos
+import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
